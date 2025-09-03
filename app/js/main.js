d02a9e92a94c0237cbbe727392648b70cb3f5e47
Cbandy Copper <gektor93@gmail.com>
2025-09-03 14:44:18 +0300

    added product section

--- app/js/main.js
+++ app/js/main.js
@@ -6,4 +6,11 @@ $(function () {
     autoplay: true,
     autoplaySpeed: 8000,
   });
+
+  $(".stars").rateYo({
+    starWidth: "17px",
+    normalFill: "#ccccce",
+    ratedFill: "#ffc35b",
+    readOnly: true,
+  });
 });
